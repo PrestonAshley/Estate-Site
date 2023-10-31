f2a9756a9c5acecaa35ff54b8e69c96c97cdea45
ashley <preash63@yahoo.com>
2023-10-31 21:20:44 +0530

    Complete Delete User Listing Functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createListing } from '../controllers/listing.conttoller.js';
+import { createListing, deleteListing } from '../controllers/listing.conttoller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
