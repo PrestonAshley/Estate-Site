b7823cf584088167cf1ab25247cb7e0069ed466c
ashley <preash63@yahoo.com>
2023-10-31 12:23:48 +0530

    Complete Create Listing Page UI

--- api/models/listing.model.js
+++ api/models/listing.model.js
@@ -30,7 +30,7 @@ const listingSchema = new mongoose.Schema(
             type: Number,
             required: true
         },
-        funished: {
+        furnished: {
             type: Boolean,
             required: true
         },
