eb3f118d72907c243c77d62bcf4edcaabc5e5ede
ashley <preash63@yahoo.com>
2023-11-01 20:57:21 +0530

    Complete About Page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Ashley Estate</h1>
+      <p className='mb-4 text-slate-700'>There are sixty-five gorgeous cards in the deck, and they are full of The Secret’s most important teachings as well as simple, powerful practices to ensure you create whatever you want—happiness, health, wealth, and anything you can dream of. The Secret Manifestation Cards provide the power of quick, daily reminders to continually improve and perfect your manifestation practice. </p>
+      <p className='mb-4 text-slate-700'>And there couldn’t be a more beautiful, affordable, or life-changing gift for all the people you love this holiday season. It just doesn’t get any better than this – all the joy and inspiration of The Secret teachings, in the most beautiful card deck and box. It doesn’t matter how much you know about manifestation, or if someone doesn’t know anything about manifesting at all, each of the card’s wisdom speaks to every person of every stage, age, and culture.</p>
+      <p className='mb-4 text-slate-700'>The Secret Manifestation Cards are sure to be a sellout, so go and grab a deck for yourself, and check off a whole bunch of very special holiday gifts at the same time! May the joy be with you, Rhonda Byrne. The Secret … bringing joy to billions. P.S. For our international friends, the English version is now available for worldwide shipping. The Secret Manifestation Cards will be released in other major languages in 2024. </p>
+    </div>
   )
 }
